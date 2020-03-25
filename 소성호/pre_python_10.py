002fd49f74bd76678c7ebf12f6b70e8f7130dbaa
CoffeeAmericano <sunghosoh@naver.com>
2020-03-25 09:30:37 +0900

    Quiz 01

---
+++ 소성호/pre_python_10.py
@@ -0,0 +1,20 @@
+"""10. 팩토리얼을 구하는 함수를 작성하시오.
+ex ) 5! = 5x4x3x2x1
+  print(factoral(5)) -> 120 출력
+  
+  
+예시
+<입력>
+print(factorial(5))
+<출력>
+120
+  """
+def factorial(a):
+    result = 1
+
+    for i in range(1,a+1):
+        result = result*i
+
+    print(result)
+
+factorial(5)
